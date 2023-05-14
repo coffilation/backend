d6075641618511154be0ab5fc0dc1dbdb192ac01
xalvaine <xalvaine@gmail.com>
2023-05-15 02:43:23 +0300

    [fix] Fix permissions

--- apps/compilations/permissions.py
+++ apps/compilations/permissions.py
@@ -5,12 +5,12 @@ from .models import CompilationPermission
 
 class CanEditCompilation(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.has_perm(CompilationPermission.CHANGE_COMPILATION, request.user)
+        return request.user.has_perm(CompilationPermission.CHANGE_COMPILATION, obj)
 
 
 class CanChangeCompilationPlaces(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.has_perm(CompilationPermission.CHANGE_PLACES_LIST, request.user)
+        return request.user.has_perm(CompilationPermission.CHANGE_PLACES_LIST, obj)
 
 
 class CanDeleteCompilation(BasePermission):
